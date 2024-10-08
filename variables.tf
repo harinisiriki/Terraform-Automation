cbc9d1fcf19ae16d39bf74cbae4be2a8b73698ca
harini <118281562+harinisiriki@users.noreply.github.com>
2024-10-08 19:22:18 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "aws-cli-pwd" 
+    default     =  "aws-lic-pwd" 
 }
 
 variable "instance_type" { 
